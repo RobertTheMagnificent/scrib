47f0b45bb002f8eb22161b823e2b58f3def17e4a
Sina Mashek <mashek@thescoundrels.net>
2013-10-13 19:35:06 -0700

    Organizing tools.

--- tools/rss_php.php
+++ tools/rss_php.php
@@ -3,7 +3,7 @@
 	RSS_PHP - the PHP DOM based RSS Parser
 	Author: <rssphp.net>
 	Published: 200801 :: blacknet :: via rssphp.net
-	
+
 	RSS_PHP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
 
 	Usage:
@@ -13,7 +13,7 @@
 */
 
 class rss_php {
-	
+
 	public $document;
 	public $channel;
 	public $items;
@@ -79,7 +79,7 @@ class rss_php {
 			$this->document = $this->extractDOM($DOMDocument->childNodes);
 		}
 	}
-	
+
 	private function valueReturner($valueBlock=false) {
 		if(!$valueBlock) {
 			$valueBlock = $this->document;
@@ -96,7 +96,7 @@ class rss_php {
 		}
 		return $valueBlock;
 	}
-	
+
 	private function extractDOM($nodeList,$parentNodeName=false) {
 		$itemCounter = 0;
 		foreach($nodeList as $values) {
@@ -107,7 +107,7 @@ class rss_php {
 				} else {
 					$nodeName = $values->nodeName;
 				}
-				$tempNode[$nodeName] = array();				
+				$tempNode[$nodeName] = array();
 				if($values->attributes) {
 					for($i=0;$values->attributes->item($i);$i++) {
 						$tempNode[$nodeName]['properties'][$values->attributes->item($i)->nodeName] = $values->attributes->item($i)->nodeValue;
@@ -136,7 +136,7 @@ class rss_php {
 		}
 		return $tempNode;
 	}
-	
+
 	private function randomContext() {
 		$headerstrings = array();
 		$headerstrings['User-Agent'] = 'Mozilla/5.0 (Windows; U; Windows NT 5.'.rand(0,2).'; en-US; rv:1.'.rand(2,9).'.'.rand(0,4).'.'.rand(1,9).') Gecko/2007'.rand(10,12).rand(10,30).' Firefox/2.0.'.rand(0,1).'.'.rand(1,9);
@@ -154,7 +154,7 @@ class rss_php {
 		);
 		return stream_context_create($contextOptions);
 	}
-	
+
 }
 
 ?>

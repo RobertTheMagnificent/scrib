00d3ef5a94b156ba7ccb32f7b914f438e15a49fd
Sina Mashek <mashek@thescoundrels.net>
2013-10-23 07:25:11 -0700

    I think...

--- plugins/echo.py
+++ plugins/echo.py
@@ -6,12 +6,12 @@ command = { "echo": "Usage: !echo message\nMake the bot mimic your message." }
 
 # Plugin Action
 class EchoPlugin(ScribPlugin.ScribPlugin):
-	def action(ModIRC, command_list):
+	def action(self, command_list):
 		if len(command_list) >= 2:
 			phrase=""
 			for x in xrange (2, len (command_list)):
 				phrase = phrase + str(command_list[x]) + " "
-			ModIRC.output(phrase, ("", command_list[1], "", c, e))
+			self.output(phrase, ("", command_list[1], "", c, e))
 		
 
 ScribPlugin.addPlugin( command, alias, EchoPlugin() )

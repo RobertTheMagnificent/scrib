8bf93f97df07cc169e6bac4eb585fdc89d9c1381
Sina Mashek <mashek@thescoundrels.net>
2013-10-23 09:31:44 -0700

    added placeholder

--- plugins/BasicIRCCommands.py
+++ plugins/BasicIRCCommands.py
@@ -110,6 +110,8 @@ class UnIgnorePlugin(ScribPlugin.ScribPlugin):
 	def action(self, command_list, scrib):
 		if command_list[0] == unignore_alias:
 			#do stuff
+			msg = "derp
+		return msg
 
 ScribPlugin.addPlugin( nick_command, nick_alias, NickPlugin() )
 ScribPlugin.addPlugin( join_command, join_alias, JoinPlugin() )

7025f50d3753e42de4b983a9b2b4b11fdf773e60
Sina Mashek <mashek@thescoundrels.net>
2013-10-23 10:25:55 -0700

    reducing time code

--- plugins/ScribPlugin.py
+++ plugins/ScribPlugin.py
@@ -3,9 +3,6 @@ process_table = {}
 plugin_commands = {}
 plugin_aliases = []
 
-def get_time():
-	return time.strftime("%H:%M:%S", time.localtime(time.time()))
-
 def addPlugin( command, alias, action ):
 	global plugin_commands, plugin_aliases
 	process_table[alias[1:]] = action

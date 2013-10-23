a6647250d5b42cfcf350a1d72846f22b7fb6fa53
Sina Mashek <mashek@thescoundrels.net>
2013-10-23 05:59:48 -0700

    more plugin shitsu

--- plugins/echo.py
+++ plugins/echo.py
@@ -1,8 +1,12 @@
 from plugins import ScribPlugin
 
+# User Alias and Command
+alias = "echo"
+command = { "!echo": "Usage: !echo message\nMake the bot mimic your message." }
+
+# Plugin Action
 class EchoPlugin(ScribPlugin.ScribPlugin):
 	def action(self, stuff):
 		return stuff
 
-ScribPlugin.plugincommands['!echo'] = "Usage: !echo message\nMake the bot mimic your message."
-ScribPlugin.addPlugin( "echo", EchoPlugin() )
+ScribPlugin.addPlugin( command, alias, EchoPlugin() )

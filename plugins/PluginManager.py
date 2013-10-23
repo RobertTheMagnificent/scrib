de7262dfe205f26a96c663187379d58418070942
Sina Mashek <mashek@thescoundrels.net>
2013-10-23 08:58:41 -0700

    Sigh

--- plugins/PluginManager.py
+++ plugins/PluginManager.py
@@ -1,7 +1,7 @@
 from plugins import ScribPlugin
 
-def sendMessage( event, text ):
+def sendMessage( event, text, scrib ):
 	if ScribPlugin.process_table[event] != '':
-		return ScribPlugin.process_table[event].action(text)
+		return ScribPlugin.process_table[event].action(text, scrib)
 	else:
 		return ''

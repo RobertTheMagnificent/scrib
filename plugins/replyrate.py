b7e3228d9c5c305adc8c73ed1d1c169a1e111974
Sina Mashek <mashek@thescoundrels.net>
2013-10-23 08:13:26 -0700

    Moved replyrate to plugin

---
+++ plugins/replyrate.py
@@ -0,0 +1,16 @@
+from plugins import ScribPlugin
+
+# User Alias and Command
+alias = "!echo"
+command = { "echo": "Usage: !echo message\nMake the bot mimic your message." }
+
+# Plugin Action
+class EchoPlugin(ScribPlugin.ScribPlugin):
+	def action(self, command_list):
+		if len(command_list) >= 1:
+			phrase=""
+			for x in xrange (1, len (command_list)):
+				phrase = phrase + str(command_list[x]) + " "
+			return phrase
+
+ScribPlugin.addPlugin( command, alias, EchoPlugin() )

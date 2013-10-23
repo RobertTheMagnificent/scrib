fb73f069145da0b74a8f1771061b61cfcebc1aff
Sina Mashek <mashek@thescoundrels.net>
2013-10-23 09:46:24 -0700

    more cleanup.

--- scrib-irc.py
+++ scrib-irc.py
@@ -38,7 +38,7 @@ class ModIRC(SingleServerIRCBot):
 	# We are going to store the owner's host mask :3
 	owner_mask = []
 
-	commandlist = " "+PluginManager.ScribPlugin.plugin_aliases
+	commandlist = dict( commandlist.items() + PluginManager.ScribPlugin.plugin_aliases.items() )
 	commanddict = dict( commanddict.items() + PluginManager.ScribPlugin.plugin_commands.items() )
 	
 	def __init__(self, my_scrib, args):

54f80baa37beff48b7aebf3c9318f30d4099c495
Sina Mashek <mashek@thescoundrels.net>
2013-10-22 21:46:31 -0700

    lsdlfkjasf;

--- core/scrib.py
+++ core/scrib.py
@@ -110,7 +110,7 @@ class scrib:
 
 		# Read the brain
 		if self.settings.process_with == "scrib":
-			print "Reading my brain..."
+			print "[#] Reading my brain..."
 			try:
 				zfile = zipfile.ZipFile('data/archive.zip','r')
 				for filename in zfile.namelist():
@@ -119,7 +119,7 @@ class scrib:
 					file.write(data)
 					file.close()
 			except (EOFError, IOError), e:
-				print "no zip found"
+				print "[!] No zip found"
 			try:
 
 				f = open("data/version", "rb")

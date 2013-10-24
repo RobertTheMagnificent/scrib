b2d4be6167b5f31213e5fb8c9f734c43fc06245b
Sina Mashek <mashek@thescoundrels.net>
2013-10-24 06:58:35 -0700

    Removing stale configs from scrib.cfg

---
+++ plugins/Twitter.py
@@ -0,0 +1,23 @@
+from plugins import ScribPlugin
+import cfgfile
+import twitter
+
+# User Alias and Command
+alias = "!tweet"
+command = { "tweet": "Owner Command. Usage: !tweet\nTweet the most recently said line." }
+
+# Plugin Action
+class TwitterPlugin(ScribPlugin.ScribPlugin):
+	def action(self, command_list, scrib):
+		if command_list[0] == alias and scrib.settings.twitter = 1:
+			self.settings = cfgfile.cfgset()
+			self.settings.load("conf/twitter.cfg",
+			{ "con_key = ": ("Consumer Key", ""),
+			  "con_secret": ("Consumer Secret", ""),
+			  "token_key":	("Access Token", ""),
+			  "token_secret":("Access Secret", "")
+			} )
+			scrib_auth = twitter.OAuth(token_key, token_seret, con_key, con_secret)
+			twit = twitter.Twitter(auth=scrib_auth)
+
+ScribPlugin.addPlugin( command, alias, TwitterPlugin() )

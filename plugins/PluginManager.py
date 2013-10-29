261c334230d1e53d2e4bd5b223ffd7a3f60d388c
Sina Mashek <mashek@thescoundrels.net>
2013-10-28 18:33:14 -0700

    Plugins are still not using pubsym...

--- plugins/PluginManager.py
+++ plugins/PluginManager.py
@@ -4,11 +4,11 @@ def sendMessage( event, text, scrib, c ):
 	if ScribPlugin.process_table[event] != '':
 		return "%s %s" % (scrib.settings.pubsym, ScribPlugin.process_table[event].action(text, scrib, c))
 	else:
-		return ''
+		return '%s Nothing to return.' % scrib.settings.pubsym
 
 def reloadPlugin( event ):
 	if ScribPlugin.process_table[event] != '':
 		reload(event)
-		return "Reloaded %s" % event
+		return "%s Reloaded %s" % (scrib.settings.pubsym, event)
 	else:
-		return 'Reloading error'
+		return '%s Reloading error' % scrib.settings.pubsym

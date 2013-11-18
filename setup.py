6308b1dd5f611f3ef596b7711329a2a8995d20cd
Sina Mashek <mashek@thescoundrels.net>
2013-11-17 18:14:39 -0800

    Now pulls version from VERSION

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
 
+from core import cfgfile
 from setuptools import setup
+
+settings = cfgfile.cfgset()
+settings.load('VERSION', 
+				{'brain': ('Brain version', ''), 
+				'core': ('Core version', '')
+				})
 
 long_description = open('README.md', 'r').read()
 
 setup(
 	name="scrib",
-	version="0.7.4",
+	version=settings.core,
 	packages=['scrib',],
 	package_data={'scrib': ['*.py'],},
 	author="Sina Mashek",
 	author_email="mashek@thescoundrels.net",
 	long_description=long_description,
-	description="learning bot"
+	description="learning bot",
 	license="GPLv2",
 	url="http://scoundrels.github.io/scrib",
 	platforms=['any']

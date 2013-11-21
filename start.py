e2d248f562d62a707f41b467dbd4b06ca3dbd71e
Tristy Hopkins <kittytristy@gmail.com>
2013-11-20 22:52:29 -0800

    Fixed some OS pathing issues

--- start.py
+++ start.py
@@ -5,30 +5,31 @@ import os
 
 if __name__ == "__main__":
 
-	def help():
-		print "Scrib bot. Usage:"
-		print "\tstart.py [options]"
-		print "\t--irc (connection info stored in conf/scrib-irc.cfg"
-		print "\t--feedme [filename] (text file to process)"
-		print "\n"
-		sys.exit(0)
+    def help():
+        print "Scrib bot. Usage:"
+        print "\tstart.py [options]"
+        print "\t--irc (connection info stored in conf/scrib-irc.cfg"
+        print "\t--feedme [filename] (text file to process)"
+        print "\n"
+        sys.exit(0)
 
-	if "--help" in sys.argv:
-		help()
-	
-	if "--irc" in sys.argv:
-		if os.path.isfile("interfaces/scrib_irc.py"):
-			os.system("interfaces/scrib_irc.py")
-		else:
-			print "No IRC interface. Exiting."
-			exit(0)
-	
-	if "--feedme" in sys.argv:
-		if sys.argv[2]:
-			if os.path.isfile("interfaces/feedme.py"):
-				os.system("interfaces/feedme.py %s" % sys.argv[2])
-		exit(0)
+    if "--help" in sys.argv:
+        help()
 
-	else:
-		os.system("interfaces/default.py")
-		exit(0)
+    if "--irc" in sys.argv:
+        if os.path.isfile(os.path.join("interfaces", "scrib_irc.py")):
+            os.system(os.path.join("interfaces", "scrib_irc.py"))
+        else:
+            print "No IRC interface. Exiting."
+            exit(0)
+
+    if "--feedme" in sys.argv:
+        if sys.argv[2]:
+            if os.path.isfile(os.path.join("interfaces", "feedme.py")):
+                os.system(os.path.join("interfaces", "feedme.py" %s) % sys.argv[2])
+        exit(0)
+
+    else:
+        os.system(os.path.join("interfaces","default.py"))
+        print os.name
+        exit(0)

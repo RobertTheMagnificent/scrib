92557e3baf73c6110030e5e1d09c2e7a0de9d081
Sina Mashek <mashek@thescoundrels.net>
2014-07-02 14:36:36 -0700

    Moving num_* to brain cfg

--- core/brain.py
+++ core/brain.py
@@ -33,7 +33,10 @@ class brain:
 			'symbol': '!',
 			'learning': 1,
 			'censored': [],
+			'num_words': 0,
+			'num_contexts': 0,
 			'num_aliases': 0,
+			'max_words': 1000000,
 			'aliases': {},
 			'optimum': 1,
 			'ignore_list': [],
@@ -45,11 +48,6 @@ class brain:
 			self.barf('ROL', 'We are working on an external conversion script.')
 			self.barf('ROL', 'Continue without upgrading.')
 
-		self.stats = {
-			"num_contexts": 0,
-			"num_words": 0,
-			"max_words": 1000000
-		}			
 		self.static_answers = self.cfg.set()
 		self.static_answers.load("brain/answers.dat", {
 							"sentences": {}
@@ -143,7 +141,6 @@ class brain:
 			s = json.loads(file)
 			if upgrade == True:
 				for v in s:
-					print 'Filtering...'
 					s[v][0] = filter(lambda x: x in string.printable, s[v][0])
 				return s
 			return s
@@ -259,13 +256,13 @@ class brain:
 			self.barf('ACT', "Optimizing brain bits...")
 			self.auto_rebuild()
 		self.barf('ACT', "Calculating words and contexts...")
-		self.stats['num_words'] = len(self.words)
+		self.settings.num_words = len(self.words)
 		num_contexts = 0
 		# Get number of contexts
 		for x in self.lines.keys():
 			num_contexts += len(self.lines[x][0].split())
-		self.stats['num_contexts'] = num_contexts
-		self.barf('ACT', "%s words and %s contexts loaded" % ( self.stats['num_words'], self.stats['num_contexts'] ))
+		self.settings.num_contexts = num_contexts
+		self.barf('ACT', "%s words and %s contexts loaded" % ( self.settings.num_words, self.settings.num_contexts ))
 		
 		# Is an aliases update required ?
 		count = 0
@@ -432,9 +429,9 @@ class brain:
 				elif ( "-" in words[x] or "_" in words[x] ):
 					words[x] = "#nick"
 
-			num_w = self.stats['num_words']
+			num_w = self.settings.num_words
 			if num_w != 0:
-				num_cpw = self.stats['num_contexts'] / float(num_w) # contexts per word
+				num_cpw = self.settings.num_contexts / float(num_w) # contexts per word
 			else:
 				num_cpw = 0
 
@@ -451,13 +448,13 @@ class brain:
 						self.words[words[x]].append([hashval, x])
 					else:
 						self.words[words[x]] = [hashval, x]
-						self.stats['num_words'] += 1
-					self.stats['num_contexts'] += 1
+						self.settings.num_words += 1
+					self.settings.num_contexts += 1
 			else:
 				self.lines[hashval][1] += num_context
 
 			#is max_words reached, don't learn more
-			if self.stats['num_words'] >= self.stats['max_words']:
+			if self.settings.num_words >= self.stats['max_words']:
 				self.settings.learning = 0
 				self.barf('ERR', "Had to turn off learning- max_words limit reached!")
 
@@ -473,13 +470,13 @@ class brain:
 			t = time.time()
 
 			old_lines = self.lines
-			old_num_words = self.stats['num_words']
-			old_num_contexts = self.stats['num_contexts']
+			old_num_words = self.settings.num_words
+			old_num_contexts = self.settings.num_contexts
 
 			self.words = {}
 			self.lines = {}
-			self.stats['num_words'] = 0
-			self.stats['num_contexts'] = 0
+			self.settings.num_words = 0
+			self.settings.num_contexts = 0
 
 			for k in old_lines.keys():
 				filtered_line = self.clean.line(old_lines[k][0], self.settings)
@@ -487,9 +484,9 @@ class brain:
 			msg = "Rebuilt brain in %0.2fs. Words %d (%+d), contexts %d (%+d)." % \
 				  (time.time() - t,
 				   old_num_words,
-				   self.stats['num_words'] - old_num_words,
+				   self.settings.num_words - old_num_words,
 				   old_num_contexts,
-				   self.stats['num_contexts'] - old_num_contexts)
+				   self.settings.num_contexts - old_num_contexts)
 
 			# Restarts the timer
 			self.autorebuild = threading.Timer(self.to_sec("71h"), self.auto_rebuild)
@@ -534,10 +531,10 @@ class brain:
 				# Check for any of the deleted contexts
 				if y[0] in dellist:
 					del word_contexts[y]
-					self.stats['num_contexts'] = self.stats['num_contexts'] - 1
+					self.settings.num_contexts = self.settings.num_contexts - 1
 			if len(words[x]) == 0:
 				del words[x]
-				self.stats['num_words'] = self.stats['num_words'] - 1
+				self.settings.num_words = self.settings.num_words - 1
 				self.barf('ACT', "\"%s\" vaporized from brain." % x)
 
 	def reply(self, body):
